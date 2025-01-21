b4488d5617b3de0ade7b780be11159a69a1c7940
khaled0195 <khaledsaifullah50956@gmail.com>
2025-01-21 20:29:29 +0600

    Add project files.

---
+++ EmployeeSalary/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace EmployeeSalary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowDetails = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShowDetails
+            // 
+            this.btnShowDetails.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowDetails.Location = new System.Drawing.Point(627, 133);
+            this.btnShowDetails.Name = "btnShowDetails";
+            this.btnShowDetails.Size = new System.Drawing.Size(149, 35);
+            this.btnShowDetails.TabIndex = 1;
+            this.btnShowDetails.Text = "show Details";
+            this.btnShowDetails.UseVisualStyleBackColor = true;
+            this.btnShowDetails.Click += new System.EventHandler(this.btnShowDetails_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCalculate);
+            this.panel1.Controls.Add(this.lblSalary);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnShowDetails);
+            this.panel1.Location = new System.Drawing.Point(12, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(786, 192);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Location = new System.Drawing.Point(12, 197);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(786, 241);
+            this.panel2.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(125, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(479, 202);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Total Salary--->";
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalary.Location = new System.Drawing.Point(171, 106);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(56, 22);
+            this.lblSalary.TabIndex = 4;
+            this.lblSalary.Text = "salary";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("MS Reference Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(29, 46);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(149, 35);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate Salary";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnShowDetails;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ EmployeeSalary/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeSalary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShowDetails_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=KHALED-0195;Initial Catalog=master;Integrated Security=True;Encrypt=False;");
+
+
+            sqlcon.Open();
+            SqlCommand sqlcom = new SqlCommand("select * from EmployeeSalaryTable;", sqlcon);
+            SqlDataAdapter sda = new SqlDataAdapter(sqlcom);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+            sqlcon.Close();
+
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                
+                var salary = Convert.ToInt32(selectedRow.Cells["salary"].Value);
+
+                var totalServiceMonth = Convert.ToInt32(selectedRow.Cells["totalServiceMonth"].Value);
+                
+
+              
+                var totalSalary = salary * totalServiceMonth;
+
+
+                this.lblSalary.Text = totalSalary.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please select a row first!" , "selection error ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+        }
+    }
+}
